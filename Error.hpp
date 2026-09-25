2648f982773f7ac22356df9fd72dc43d1c3a78b7
file.cpp <file.cpp@proton.me>
2026-09-25 22:53:41 +0200

    Refactor THROW_ERROR macro for better readability

--- Error.hpp
+++ Error.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <stdexcept>
 #include <string>
 
-#define THROW_ERROR(msg) \
-	throw std::runtime_error(std::string("Error: ") + msg + " (in file " + __FILE__ + ", line " + std::to_string(__LINE__) + ")")
+#define THROW_ERROR(msg)							\
+	throw											\
+		std::runtime_error(std::string("Error: ") + \
+		msg +										\
+		" (in file " + __FILE__ +					\
+		", line " + \ std::to_string(__LINE__) +	\
+		")")
