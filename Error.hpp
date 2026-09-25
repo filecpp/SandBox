6bbbc5e54b5949bf3aa46650738634dc85757bfc
file.cpp <file.cpp@proton.me>
2026-09-25 18:22:00 +0200

    Add error handling macro in Error.hpp

---
+++ Error.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+
+#define THROW_ERROR(msg) \
+	throw std::runtime_error(std::string("Error: ") + msg + " (in file " + __FILE__ + ", line " + std::to_string(__LINE__) + ")")
