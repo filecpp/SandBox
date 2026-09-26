cf491b47b64da72f7fc13720ce9c6291dbfbd847
file.cpp <file.cpp@proton.me>
2026-09-26 16:43:00 +0200

    Add clear and is_empty methods to Space class
    Added clear method to reset cell values and updated is_empty method to check for empty cells.

--- Space.hpp
+++ Space.hpp
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <vector>
 
+// Stores all cells of the space
 class Space final {
 private:
     std::vector<std::int8_t> cells;
@@ -20,11 +21,24 @@ public:
         cells.resize(width * height);
     }
 
+    void clear() noexcept {
+        for (auto& cell : cells)
+            cell = -1;
+    }
+
+    // Checks if the cell is empty
+    bool is_empty(Vector2 position) const noexcept {
+        return cells[position.y * width + position.x] ==
+            static_cast<std::int8_t>(material_translator::empty);
+    }
+
+    // Adds a material to the specified cell
     void add(Vector2 position, material_translator material) {
         cells[position.y * width + position.x] =
             static_cast<std::int8_t>(material);
     }
 
+    // Moves a material from one cell to another
     void move(Vector2 position, Vector2 target_position) {
         const auto source = position.y * width + position.x;
         const auto target = target_position.y * width + target_position.x;
@@ -35,6 +49,16 @@ public:
         cells[source] = -1;
     }
 
+    // Checks if the position is within the space
+    [[nodiscard]]
+    bool in_bounds(Vector2 p) const noexcept {
+        return p.x >= 0 &&
+            p.x < width &&
+            p.y >= 0 &&
+            p.y < height;
+    }
+
+    // Returns all cells of the space
     [[nodiscard]]
     const std::vector<std::int8_t>& get_space() const noexcept {
         return cells;
