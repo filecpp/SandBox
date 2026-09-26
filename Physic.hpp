86b8150bd01b70d8c67f5a07ea6878007f0c571c
file.cpp <file.cpp@proton.me>
2026-09-26 16:38:34 +0200

    Add physic function for space physics calculations

---
+++ Physic.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Space.hpp"
+#include "MaterialTranslation.hpp"
+#include "Materials.hpp"
+
+#include <iostream>
+
+int physic(Space& space) {
+	// Copying the current space matrix for physics calculations
+	std::vector space_matrix{ space.get_space() };
+
+	// Going through the whole matrix from bottom to top
+	for (int32_t y = space.height - 1; y >= 0; y--) {
+		for (int32_t x = space.width - 1; x >= 0; x--) {
+			Vector2 position(x, y);
+
+			// Skip the cell if it is empty
+			if (space.is_empty(Vector2(x, y)))
+				continue;
+
+			// Checking if the current cell contains sand
+			if (space_matrix[y * space.width + x] ==
+				static_cast<std::int8_t>(material_translator::sand)) {
+
+				material_registry.at(
+					static_cast<int8_t>(material_translator::sand))->behavior(space, position, Vector2(space.width, space.height));
+			}
+			// Checking if the current cell contains water
+			else if (space_matrix[y * space.width + x] ==
+				static_cast<std::int8_t>(material_translator::water)) {
+
+				material_registry.at(
+					static_cast<int8_t>(material_translator::water))->behavior(space, position, Vector2(space.width, space.height));
+			}
+			// Checking if the current cell contains wood
+			else if (space_matrix[y * space.width + x] ==
+				static_cast<std::int8_t>(material_translator::wood)) {
+
+				material_registry.at(
+					static_cast<int8_t>(material_translator::wood))->behavior(space, position, Vector2(space.width, space.height));
+			}
+		}
+	}
+}
