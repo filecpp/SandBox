67a4c2f586d35f2234625097f6f40bbe26a197a9
file.cpp <file.cpp@proton.me>
2026-09-26 12:56:24 +0200

    Change std::int8_t to int8_t in enum class

--- MaterialTranslation.hpp
+++ MaterialTranslation.hpp
@@ -3,8 +3,8 @@
 #include <cstdint>
 
 enum class material_translator : std::int8_t {
-	empty = std::int8_t{ -1 },
-	sand = std::int8_t{ 0 },
-	water = std::int8_t{ 1 },
-	wood = std::int8_t{ 2 },
+	empty = int8_t{ -1 },
+	sand = int8_t{ 0 },
+	water = int8_t{ 1 },
+	wood = int8_t{ 2 },
 };
